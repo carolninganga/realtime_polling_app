70589e8593ee29ca5fbb452c601337c33fe43c92
Caroline Ninganga <carolninganga@gmail.com>
2020-06-05 15:43:34 -0400

    added OS Results chart

---
+++ public/main.js
@@ -0,0 +1,45 @@
+const form = document.getElementById('vote-form');
+
+form.addEventListener('submit', (e) => {
+    const choice = document.querySelector('input[name=os]:checked').value;
+    const data = {os: choice};
+
+    fetch('http://localhost:8080/poll', {
+        method: 'post',
+        body: JSON.stringify(data),
+        headers: new Headers({
+            'Content-Type': 'application/json'
+        })
+    })
+        .then(res => res.json())
+        .then(data => console.log(data))
+        .catch(err => console.log(err));
+
+    e.preventDefault();
+});
+
+let dataPoints = [
+    { label: 'Windows', y: 0 },
+    { label: 'MacOs', y: 0 },
+    { label: 'Linux', y: 0 },
+    { label: 'Other', y: 0 }
+];
+
+const charContainer = document.querySelector('#chartContainer');
+
+if(charContainer) {
+    const chart = new CanvasJS.Chart('chartContainer', {
+        animationEnabled: true,
+        theme: 'theme1',
+        title: {
+            text: 'OS Results'
+        },
+        data: [
+            {
+                type: 'column',
+                dataPoints: dataPoints
+            }
+        ]
+    });
+    chart.render();
+}
